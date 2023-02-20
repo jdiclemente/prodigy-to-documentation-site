dc24ea6c1ef46b9e41012c0ff88a0388796592ca
jdiclemente <30302506+jdiclemente@users.noreply.github.com>
2023-02-20 17:04:51 -0500

    update form ids

--- src/pages/docs/contactSales.jsx
+++ src/pages/docs/contactSales.jsx
@@ -10,8 +10,8 @@ export default function App() {
       <p>Our sales team is always happy to answer any questions you might have. If you provide us with the total number of clinitians you have on staff, we can supply you with a quote to get us started. Looking to a demo? Use this calander link to <Link href="https://meetings.hubspot.com/ryan1574">schedule your demo</Link> today.</p>
     </div>
       <HubspotForm
-        portalId="4753378"
-        formId="c35e528c-5f90-4bd6-8c5a-fe4e6e6eeb1e"
+        portalId={process.env.NEXT_PUBLIC_PORTALID}
+        formId={process.env.NEXT_PUBLIC_FORMID}
         onSubmit={() => console.log("Submit!")}
         onReady={(form) => console.log("Form ready!")}
         loading={<div></div>}
